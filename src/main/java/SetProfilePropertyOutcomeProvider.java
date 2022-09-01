8adee5b2b710b742089c3314a7c31c777cd80654
farodia <2267067686@qq.com>
2022-09-01 20:21:06 +0800

    feat: implement outcome provider

--- src/main/java/SetProfilePropertyOutcomeProvider.java
+++ src/main/java/SetProfilePropertyOutcomeProvider.java
@@ -1,3 +1,4 @@
+import com.google.common.collect.ImmutableList;
 import org.forgerock.json.JsonValue;
 import org.forgerock.openam.auth.node.api.NodeProcessException;
 import org.forgerock.openam.auth.node.api.OutcomeProvider;
@@ -6,8 +7,12 @@ import org.forgerock.util.i18n.PreferredLocales;
 import java.util.List;
 
 public class SetProfilePropertyOutcomeProvider implements OutcomeProvider {
+
+    public static final String OUTCOME_ID = "OUTCOME";
+    public static final String DISPLAY_NAME = "SetProfilePropertyOutcome";
+
     @Override
     public List<Outcome> getOutcomes(PreferredLocales preferredLocales, JsonValue jsonValue) throws NodeProcessException {
-        return null;
+        return ImmutableList.of(new Outcome(OUTCOME_ID, DISPLAY_NAME));
     }
 }
